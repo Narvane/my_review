871fe6b7b8bcefa965ddba15c12adc4c667e093e
narvane <gustavoalvesandrade18@gmail.com>
2019-12-09 20:40:50 -0200

    att3

---
+++ myreview-ui/src/app/establishment.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EstablishmentService {
+
+  apiurl = 'http://localhost:8080/establishments';
+  
+  constructor(private httpCliente: HttpClient) { }
+
+  list() {
+    return this.httpCliente.get(this.apiurl);
+  }
+
+  getEstablishmentsByState(stateName: string) {
+    return this.httpCliente.get(this.apiurl + "/" + stateName);
+  }
+
+  getEstablishmentsByCity(stateName: string, cityName: string) {
+    return this.httpCliente.get(this.apiurl + "/" + stateName + "/" + cityName);
+  }
+
+  getEstablishmentsByDistrict(stateName: string, cityName: string, districtName: string) {
+    return this.httpCliente.get(this.apiurl + "/" +  stateName + "/" +  cityName + "/" +  districtName);
+  }
+
+  getStateList() {
+    return this.httpCliente.get(this.apiurl + "/states");
+  }
+  getCitiesList(stateName: string) {
+      return this.httpCliente.get(this.apiurl + "/" + stateName + "/cities");
+  }
+  getDestrictList(stateName: string, cityName: string) {
+    return this.httpCliente.get(this.apiurl + "/" + stateName + "/" + cityName + "/districts");
+  }
+  getEstablishment(idEstablishent){
+    return this.httpCliente.get(this.apiurl + "/establishment/" + idEstablishent);
+  }
+  getReviews(idEstablishent){
+    return this.httpCliente.get(this.apiurl + "/reviews/" + idEstablishent);
+  }
+  getUser(idReview){
+    return this.httpCliente.get(this.apiurl + "/review/" + idReview);
+  }
+}
---
+++ myreview-ui/src/app/establishment/establishment.component.ts
@@ -0,0 +1,29 @@
+import { EstablishmentService } from './../establishment.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-establishment',
+  templateUrl: './establishment.component.html',
+  styleUrls: ['./establishment.component.css']
+})
+export class EstablishmentComponent implements OnInit {
+  public userId;
+  establishment;
+  reviews = [];
+  idUsuario;
+  user;
+
+  constructor(private establishmentService: EstablishmentService, private route: ActivatedRoute) { 
+    this.route.params.subscribe(params => this.userId = params['id'])
+  }
+
+  ngOnInit() {
+    this.establishmentService.getEstablishment(this.userId)
+    .subscribe(response => this.establishment = <any> response)
+
+    this.establishmentService.getReviews(this.userId)
+    .subscribe(response => this.reviews = <any> response)
+  }
+
+}
